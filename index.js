b872fd78135bc3f9c8adca645cf0a154b8c82d94
SxZuu <53347587+SxZuu@users.noreply.github.com>
2019-10-14 18:11:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ client.on('message', msg => {
 
   if (msg.content === '++help') {
 
-  channel.send(exampleEmbed);
+  msg.channel.send(exampleEmbed);
 
   }
   client.on ("guildMemberAdd", member =>{
